d6e209a030d7a24c6b31fbcb0a4860ceddf3ca6a
Evgeniy Berendyaev <evgeniy.berendyaev@gmail.com>
2020-04-12 22:35:42 +0300

    Неправильный путь?

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -7,7 +7,7 @@ resource "yandex_storage_bucket" "s2t" {
 resource "yandex_storage_object" "sample" {
   bucket     = yandex_storage_bucket.s2t.id
   key        = "sample"
-  source     = "../input/sample.opus"
+  source     = "input/sample.opus"
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
 }
