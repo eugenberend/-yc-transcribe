b961c55ff0aa1203649705aeb4ca1b292f66f48e
Evgeniy Berendyaev <evgeniy.berendyaev@gmail.com>
2020-04-12 20:24:57 +0300

    Ошибка: для s3 явно указывать регион

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,7 @@ terraform {
     endpoint   = "storage.yandexcloud.net"
     bucket     = "gitlab-runner-tfstate"
     key        = "gitlab/yc-transcribe.tfstate"
+    region     = "us-east-1"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
